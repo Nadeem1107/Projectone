ca29571ce214804869c023dcce2197f42236ce53
Nadeem1107 <nadeemofficial11@gmail.com>
2023-03-03 10:54:30 +0530

    conflict resolved

--- src/org/sample/Sample1.java
+++ src/org/sample/Sample1.java
@@ -2,11 +2,9 @@ package org.sample;
 
 public class Sample1 {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Hi");
-=======
+		
 		System.out.println("Hello");
->>>>>>> da76c4f9194d279e7bc9882e4df4605769eacf3d
+
 	}
 
 }
